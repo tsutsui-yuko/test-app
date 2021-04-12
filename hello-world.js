b8aefdfb4e18d4b688f37af6099786a47c08f0ac
筒井友羽子 <yoko.tsutsui@webcrew.co.jp>
2021-04-12 14:33:28 +0900

    hello world を別で生やしてみたい

--- hello-world.js
+++ hello-world.js
@@ -1,13 +1,10 @@
 'use strict';
 
-var fs = require('fs');
-var path = require('path');
 
 exports.get = function(event, context, callback) {
-  var contents = fs.readFileSync(`public${path.sep}index.html`);
   var result = {
     statusCode: 200,
-    body: contents.toString(),
+    body: "hello world",
     headers: {'content-type': 'text/html'}
   };
 
