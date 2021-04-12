eeed2211296d598b5cc282c82d463ffb9bcec9e7
筒井友羽子 <yoko.tsutsui@webcrew.co.jp>
2021-04-12 10:15:21 +0900

    hello world

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 'use strict';
 
-var fs = require('fs');
-var path = require('path');
-
 exports.get = function(event, context, callback) {
-  var contents = fs.readFileSync(`public${path.sep}index.html`);
   var result = {
     statusCode: 200,
-    body: contents.toString(),
+    body: "Congratulations Hello Codestar",
     headers: {'content-type': 'text/html'}
   };
 
